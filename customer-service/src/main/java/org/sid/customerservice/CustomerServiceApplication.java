9e95d77a9ed3c94409f994ab929d5401ca387394
Wael-BenAbid <waelbenabid1@gmail.com>
2024-10-23 12:25:45 +0100

    Update CustomerServiceApplication.java
    exemple

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -6,25 +6,34 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomerServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
+
     @Bean
     CommandLineRunner start(CustomerRepository customerRepository) {
         return args -> {
-            customerRepository.save(new Customer(null, "eni", "contact@eni.tn"));
-            customerRepository.save(new Customer(null, "FST", "contact@fst.tn"));
-            customerRepository.save(new Customer(null, "ENSI", "contact@ensi.tn"));
+            try {
+                Customer customer1 = customerRepository.save(new Customer(null, "eni", "contact@eni.tn"));
+                Customer customer2 = customerRepository.save(new Customer(null, "FST", "contact@fst.tn"));
+                Customer customer3 = customerRepository.save(new Customer(null, "ENSI", "contact@ensi.tn"));
 
-            customerRepository.findAll().forEach(customer -> {
-                System.out.println(customer);
-            });
+                logger.info("Clients ajoutés avec succès !");
+
+                logger.info("Customer 1 ID: {}", customer1.getId());
+                logger.info("Customer 2 ID: {}", customer2.getId());
+                logger.info("Customer 3 ID: {}", customer3.getId());
+            } catch (Exception e) {
+                logger.error("Erreur lors de l'ajout des clients : ", e);
+            }
         };
-
     }
-
 }
